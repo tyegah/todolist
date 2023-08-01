f315ff786d5f1bfef5efc3b3a10c127e10a70361
Tity.S <tity.septiani@gmail.com>
2023-08-01 11:18:15 +0700

    Remove storyboard and setup Navigation controller as main entry point

---
+++ ToDoList/Main/MainContentView.swift
@@ -0,0 +1,20 @@
+//
+//  MainContentView.swift
+//  ToDoList
+//
+//  Created by Ty Septiani on 01/08/23.
+//
+
+import SwiftUI
+
+struct MainContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainContentView()
+    }
+}
