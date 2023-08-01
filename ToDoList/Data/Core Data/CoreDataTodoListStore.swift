f7df17c642de6fd5014ceb2d1b1c8e8f21271b2e
Tity.S <tity.septiani@gmail.com>
2023-08-01 11:44:47 +0700

    Create TodoListStore interface and its CoreData implementation for insert function

---
+++ ToDoList/Data/Core Data/CoreDataTodoListStore.swift
@@ -0,0 +1,60 @@
+//
+//  CoreDataTodoListStore.swift
+//  ToDoList
+//
+//  Created by Ty Septiani on 01/08/23.
+//
+
+import CoreData
+
+final class CoreDataTodoListStore: TodoListStore {
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+    
+    var viewContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    init(storeURL: URL, bundle: Bundle = Bundle.main) throws {
+        container = try NSPersistentContainer.load(modelName: "ToDoList",
+                                                   url: storeURL,
+                                                   in: bundle)
+        let description = NSPersistentStoreDescription(url: storeURL)
+        description.shouldMigrateStoreAutomatically = true
+        description.shouldInferMappingModelAutomatically = false
+        container.persistentStoreDescriptions = [description]
+        context = container.newBackgroundContext()
+    }
+    
+    
+    func update(_ item: LocalTodoItem, completion: @escaping UpdateCompletion) {
+        
+    }
+    
+    func delete(_ predicate: NSPredicate?, completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    func insert(_ item: LocalTodoItem, completion: @escaping InsertionCompletion) {
+        let context = self.context
+
+        context.perform {
+            completion( Result {
+                let todoItem = TodoItem(context: context)
+                
+                todoItem.id = UUID()
+                todoItem.title = item.title
+                todoItem.desc = item.desc
+                todoItem.dueDate = item.dueDate
+                todoItem.completed = item.completed
+                todoItem.removed = item.removed
+                
+                try context.save()
+            })
+        }
+    }
+    
+    func retrieve(_ predicate: NSPredicate?, completion: @escaping RetrievalCompletion) {
+        
+    }
+}
